ca6eb8af79c116f9f5e69867c7d0e8db4a95985b
kamalakars04 <kamalakars04@gmail.com>
2020-10-31 23:17:10 +0530

    [Kamalakar].[Refactor].Added UC1_CalculateFare

---
+++ InvoiceException.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="fileName.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Your name"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace CabInvoiceGenerator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class InvoiceException : Exception
+    {
+        /// <summary>
+        /// Enum For Exception type.
+        /// </summary>
+        public enum ExceptionType
+        {
+            INVALID_RIDE_TYPE,
+            INVALID_DISTANCE,
+            INVALID_TIME,
+            NULL_RIDES,
+            INVALID_USER_ID
+        }
+
+        public ExceptionType type;
+        /// <summary>
+        /// Parameter Constructor For Setting Exception type And Throwing Exception.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="message"></param>
+        public InvoiceException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}

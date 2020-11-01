84fa9b13b5a1094b475159de13aaaf050753e402
kamalakars04 <kamalakars04@gmail.com>
2020-11-01 10:30:45 +0530

    [Kamalakar].[Refactor].Added UC2_CalculateFareForMultipleRides

--- NUnitTestCabInvoicegenerator/UnitTest1.cs
+++ NUnitTestCabInvoicegenerator/UnitTest1.cs
@@ -8,6 +8,7 @@ namespace NUnitTestCabInvoicegenerator
 {
     using NUnit.Framework;
     using CabInvoiceGenerator;
+    using System.Collections.Generic;
 
     public class Tests
     {
@@ -27,6 +28,23 @@ namespace NUnitTestCabInvoicegenerator
             Ride ride = new Ride(5, 15);
             double totalFare = generator.CalculateFare(ride);
             double expected = 65;
+            Assert.AreEqual(expected, totalFare);
+        }
+
+        /// <summary>
+        /// Whens the given distance and time return fare for multiple rides.
+        /// </summary>
+        [Test]
+        public void WhenGivenDistanceAndTimeReturnFareForMultipleRides()
+        {
+            // Create list for multiple rides
+            List<Ride> rides = new List<Ride>();
+            rides.Add(new Ride(5, 15));
+            rides.Add(new Ride(5, 15));
+
+            // Calculate fare for multiple rides
+            double totalFare = generator.CalculateFare(rides);
+            double expected = 130;
             Assert.AreEqual(expected, totalFare);
         }
     }
